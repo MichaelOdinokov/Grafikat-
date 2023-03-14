b3373bb03199a8becd63c9c2c98b24333aebb907
opilane <opilane@HP-CZC2349HTM>
2023-03-14 15:22:12 +0200

    12

--- Grafikatöö.py
+++ Grafikatöö.py
@@ -1,43 +1,53 @@
-from tkinter import*
-from Omamodul import*
+from tkinter import *
+from math import sqrt
+
+def solver(a,b,c):
+
+    D = b*b - 4*a*c
+    if D >= 0:
+        x1 = (-b + sqrt(D)) / (2*a)
+        x2 = (-b - sqrt(D)) / (2*a)
+        text = "The discriminant is: %s \n X1 is: %s \n X2 is: %s \n" % (D, x1, x2)
+    else:
+        text = "The discriminant is: %s \n This equation has no solutions" % D
+    return text
+
+def inserter(value):
+    output.delete("0.0","end")
+    output.insert("0.0",value)
 
 
+root = Tk()
+root.title("Квадратное уравнения")
 tekst="Aken"
 aken=Tk()
-aken.geometry("900x720")# razreshenija okna
+aken.geometry("500x700")# razreshenija okna
 aken.title()#zagalovak
-
 
 pealkiri=Label(aken, 
                text="Решение квадратного уравнения",
                bg="light blue", 
-               fg="green",
+               fg="#fa9302",
                font="Algerian 20",
                height=3,
-               width=50)# label raspredljaem tekst fg palitra zvetov,  font=(nazvanija shrifta, Forte)
-
-raam=Canvas(aken,
-            width=260,
-            height=260,
-            bg="black")
+               width=23)
 
 
+root.minsize(325,230)
+root.resizable(width=False, height=False)
+frame = Frame(root)
+frame.grid()
+a =Entry(frame, width=3)
+a.grid(row=1,column=1,padx=(10,0))
+a_lab = Label(frame, text="x**2+").grid(row=1,column=2)
+b = Entry(frame, width=3)
+b.grid(row=1,column=3)
+b_lab = Label(frame, text="x+").grid(row=1, column=4)
+c = Entry(frame, width=3)
+c.grid(row=1, column=5)
+c_lab = Label(frame, text="= 0").grid(row=1, column=6)
+but = Button(frame, text="Otsustama").grid(row=1, column=7, padx=(10,0))
+output = Text(frame, bg="lightblue", font="Arial 12", width=35, height=10)
+output.grid(row=2, columnspan=8)
 
-tekst_kast=Entry(aken,
-                 fg="green",
-                 bg="light blue",
-                 font="Algerian 20",
-                 width=2,
-                 justify=LEFT)
-tekst_kast.bind("<Return>", text_to_lbl)
-
-
-
-
-
-
-pealkiri.pack()# upakovka elementov
-tekst_kast.pack()#side=LEFT, RIGHT
-
-raam.pack()
-aken.mainloop()# v samom konze
+root.mainloop()
--- Omamodul.py
+++ Omamodul.py
@@ -1,4 +1,1 @@
-def text_to_lbl(event):
-    text=tekst_kast.get()
-    pealkiri.configure(text=text)
-    tekst_kast.delete(0,END)
+
