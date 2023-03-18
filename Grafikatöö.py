79971577b2ed498aa1f81bc37087e43e83d325b2
Michael Odinokov <mihhail.odinokov@gmail.com>
2023-03-18 18:05:07 +0200

    12

--- Grafikatöö.py
+++ Grafikatöö.py
@@ -1,60 +1,73 @@
 from tkinter import*
 from Omamodul import*
 
-def klikker():
-    global k
-    k+=1
-    nupp.configure(text=k)#menjaet parametri
-
 def text_to_lbl(event):
     text=tekst_kast.get()
     pealkiri.configure(text=text)
     tekst_kast.delete(0,END)
 
+def klikker():
+    global k
+    k+=1
+    nupp.configure(text=k)
+    
+
+
 tekst="Aken"
 aken=Tk()
-aken.geometry("1080x720")# razreshenija okna
-aken.title()#zagalovak
+Tk="Ruutvõrrandid"
+aken.geometry("600x720")
+aken.title()
+
 
 pealkiri=Label(aken, 
-               text="Решение квадратного уравнения",
+               text="Ruutvõrrandi lahendamine",
                bg="light blue", 
                fg="green",
                font="Algerian 20",
-               height=-3,
-               width=23)# label raspredljaem tekst fg palitra zvetov,  font=(nazvanija shrifta, Forte)
-
-raam=Canvas(aken,
-            width=300,
-            height=400,
-            bg="black")
-
-tekst_kast=Entry(aken,
-                 fg="green",
-                 bg="light blue",
-                 font="Algerian 20",
-                 width=20,
-                 justify=CENTER)
-tekst_kast.bind("<Return>")# sobitija , a potom funktsuju return=enter
-
+               height=3,
+               width=23)
 tekst_kast=Entry(aken,
                  fg="green",
                  bg="light blue",
                  font="Algerian 20",
                  width=2,
                  justify=CENTER)
-tekst_kast.bind("<Return>")# sobitija , a potom funktsuju return=enter
+tekst_kast.bind("<Return>", text_to_lbl)
+
+tekst_kas=Entry(aken,
+                 fg="green",
+                 bg="light blue",
+                 font="Algerian 20",
+                 width=2,
+                 justify=CENTER)
+tekst_kas.bind("<Return>", text_to_lbl)
+
+tekst_ka=Entry(aken,
+                 fg="green",
+                 bg="light blue",
+                 font="Algerian 20",
+                 width=2,
+                 justify=CENTER)
+tekst_ka.bind("<Return>", text_to_lbl)
 
 nupp=Button(aken,
-            text="Vajuta mind",            
-            fg="#836aa1",
+            text="Lahendada",
+            bg="green",
+            fg="black",
             font="Algerian 20",
-            activebackground="green",
-            height=3,
-            width=20,
-            command=klikker)# delaem knopku. Command standartnaja funktsija
+            height=5,
+            width=10,
+            command=klikker)
 
-pealkiri.pack()# upakovka elementov
-tekst_kast.pack()#side=LEFT, RIGHT
-raam.pack()
-aken.mainloop()# v samom konze
+
+
+pealkiri.pack()
+tekst_kast.pack()
+tekst_kas.pack() 
+tekst_ka.pack()
+nupp.pack()
+
+
+
+aken.mainloop()
